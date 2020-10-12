c03b41e836875c1eaac3afa118de4de977e15de5
Daniel <56950335+thedank_night@users.noreply.github.com>
2020-10-12 10:49:49 -0700

    Setup basic Bootstrap SCSS
    - Installed Bootstrap 4.5.2 from source, and react-bootstrap with npm. Bootstrap
      scss files are in style/scss/bootstrap and imported into style/scss/main.scss.
    
    - Installed sass and setup Node script watchSass to compile style/scss/main.scss
      to style/main.css.
    
    - Deleted default css files and changed App from functional component to a
      class extending Component.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './style/main.css';
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <h1>Hello, world!</h1>
+          </Col>
+        </Row>
+      </Container>
+    )
+  };
+};
