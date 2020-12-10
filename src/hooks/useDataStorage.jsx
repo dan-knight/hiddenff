ac1740811e7a6f0e1977a6465d8afd206f3d4070
Daniel <56950335+thedank_night@users.noreply.github.com>
2020-12-10 03:00:08 -0800

    Create useDataStorage hook to handle updating data from Axios requests
    - Hook allows for updating and adding to the existing data (appending) or
      replacing data (for example, when changing positions or sort parameters).

---
+++ src/hooks/useDataStorage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function useDataStorage() {
+  const [data, setData] = useState([]);
+
+  function update(newData) {
+    setData([...data, newData]);
+  };
+
+  function replace(newData) {
+    setData(newData);
+  };
+
+  return [data, update, replace];
+};
