6c57ad4af060858e9f3fe1d6bb56a52ec5e51c05
Daniel <56950335+thedank_night@users.noreply.github.com>
2020-12-13 00:36:02 -0800

    Change App component to functional component
    - Removed player data handling/loading from App component (and moved into
      components responsible for displaying information).

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { PositionMenu } from './components/Menus';
 import PlayerTable from './components/PlayerTable';
@@ -8,85 +8,35 @@ import { getPlayers } from './requests';
 
 import './style/main.css';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+export default function App() {
+  const [position, setPosition] = useState(['RB']);
+  const [sortBy, setSortBy] = useState('last');
+  const [showMenu, setShowMenu] = useState(false);
 
-    this.state = {
-      loading: true,
-      position: ['RB'],
-      orderBy: 'last',
-      showMenu: false,
-      data: []
-    };
+
+  function toggleMenu() {
+    setShowMenu(!showMenu);
   };
 
-  componentDidMount() {
-    if (this.state.loading) {
-      this.updateData();
-    };
+  function changePosition(value) {
+    setPosition([value]);
   };
 
-  componentDidUpdate() {
-    if (this.state.loading) {
-      this.updateData();
-    };
+  function changeSortBy(value) {
+    setSortBy(value);
   };
 
-  updateData = async () => {
-    const players = await this.getPlayerData();
-    
-    this.setState(prevState => ({ 
-      loading: false,
-      data: prevState.data.concat(players) 
-    }));
-  };
-
-  async getPlayerData() {
-    const response = await getPlayers(this.state.data.length, this.state.orderBy, this.state.position);
-    return response;
-  };
-
-  loadMorePlayers = () => {
-    this.setState(() => ({ loading: true }));
-  };
-
-  toggleMenu = () => {
-    this.setState(prevState => ({ showMenu: !prevState.showMenu }));
-  };
-
-  setPosition = value => {
-    this.setState(() => ({ 
-      loading: true,
-      position: value,
-      data: []
-    }));
-  };
-
-  setSortBy = value => {
-    this.setState(() => ({
-      loading: true,
-      orderBy: value,
-      data: []
-    }));
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <TopNav color='light' onToggle={this.toggleMenu}/>
-        <Container fluid={this.state.showMenu}>
-          <Row>
-            {this.state.showMenu ? <Col md={3}><PositionMenu value={this.state.position} onChange={this.setPosition} /></Col> : null}
-            <Col md={this.state.showMenu ? 9 : null}>
-              <PlayerTable position={this.state.position} sortBy={this.state.orderBy} onSort={this.setSortBy} />
-              <div align="center">
-                <Button onClick={this.loadMorePlayers}>Show More</Button>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </React.Fragment>
-    )
-  };
+  return (
+    <React.Fragment>
+      <TopNav color='light' onToggle={toggleMenu}/>
+      <Container fluid={showMenu}>
+        <Row>
+          {showMenu ? <Col md={3}><PositionMenu value={position} onChange={changePosition} /></Col> : null}
+          <Col md={showMenu ? 9 : null}>
+            <PlayerTable position={position} sortBy={sortBy} onSort={changeSortBy} />
+          </Col>
+        </Row>
+      </Container>
+    </React.Fragment>
+  );
 };
